6911625846faca5ad9adf663ab410738d66b46e3
oyjh <oyjh.1994@gmail.com>
2017-05-27 22:32:38 +0900

    Update 326_Power_of_Three.py

--- 326_Power_of_Three.py
+++ 326_Power_of_Three.py
@@ -7,12 +7,12 @@ import math
 
 class Solution(object):
     def __init__(self):
-        self.__max_log3 = int(math.log(0x7fffffff) / math.log(3))
-        self.__max_pow3 = 3 ** self.__max_log3
+        self.__maxLog3 = int(math.log(0x7fffffff) / math.log(3))
+        self.__maxPow3 = 3 ** self.__maxLog3
 
     def isPowerOfThree(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        return n > 0 and self.__max_pow3 % n == 0
+        return n > 0 and self.__maxPow3 % n == 0
